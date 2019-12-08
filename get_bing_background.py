7225406375e7b5b2753480e89db7dbc486896b6e
Randy Yeung <h0riz0n.yeung@gmail.com>
2019-12-08 16:45:00 -0600

    initial file drop with basic working functionality

---
+++ get_bing_background.py
@@ -0,0 +1,34 @@
+import requests
+import pprint
+import json
+from urllib import parse
+from os import path
+
+# TODO: Only download when file doesn't exist
+
+pp = pprint.PrettyPrinter(indent=4)
+
+r = requests.get("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
+
+# pp.pprint(r.json())
+
+data = json.loads(r.text)
+
+url = "http://www.bing.com" + data["images"][0]["url"]
+
+print(url)
+
+# TODO: Append date
+filename = parse.parse_qs(parse.urlparse(url).query)['id'][0]
+
+homeFolder = path.expanduser("~")
+
+# Save pictures to a folder
+pictureLocation = homeFolder + "/Pictures/"
+
+r = requests.get(url)
+
+with open(pictureLocation + filename, 'wb') as f:
+    f.write(r.content)
+
+print(pictureLocation + filename)
